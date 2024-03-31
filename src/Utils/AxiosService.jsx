eb178cad1894bd68c39c09f209362511b4d5b258
Dharanish <120582836+Dharanish3@users.noreply.github.com>
2024-03-31 16:42:51 +0530

    Update AxiosService.jsx

--- src/Utils/AxiosService.jsx
+++ src/Utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosService = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://shopy-mci9.onrender.com",
 
   headers: { "content-type": "application/json" },
 });
